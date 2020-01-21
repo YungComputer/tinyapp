03a4adb701bc086e35cf56647acd7597ef698254
CLARE NOLAN <clarenolan92@gmail.com>
2020-01-21 00:11:51 +0000

    added a second route and template

--- express_server.js
+++ express_server.js
@@ -5,13 +5,19 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
